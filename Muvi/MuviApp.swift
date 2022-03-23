022efcac371ca6b2f89701535b5f30dbb14250fb
Ibrahima Ciss <bionik6@gmail.com>
2022-03-23 07:05:48 +0000

    Initial Commit

---
+++ Muvi/MuviApp.swift
@@ -0,0 +1,20 @@
+//
+//  MuviApp.swift
+//  Muvi
+//
+//  Created by Ibrahima Ciss on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MuviApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
