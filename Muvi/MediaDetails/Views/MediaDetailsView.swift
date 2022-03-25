eef99dba9d25bb34dcc0578767c2678b6064ff89
Ibrahima Ciss <bionik6@gmail.com>
2022-03-25 23:01:46 +0000

    Update MediaDetailsView

--- Muvi/MediaDetails/Views/MediaDetailsView.swift
+++ Muvi/MediaDetails/Views/MediaDetailsView.swift
@@ -3,7 +3,7 @@ import YouTubePlayerKit
 
 struct MediaDetailsView: View {
   
-  @State private var selection: Int = 1
+  @State private var selection: Int = 1    
   @State private var playTrailer: Bool = false
   @State private var selectedClip: Clip? = nil
   @ObservedObject var viewModel: MediaDetailsViewModel
