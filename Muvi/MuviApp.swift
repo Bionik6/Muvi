ffbcd034c067d3abb92aa773d243850c79243691
Ibrahima Ciss <bionik6@gmail.com>
2022-03-26 03:47:50 +0000

    Reformat MuviApp file

--- Muvi/MuviApp.swift
+++ Muvi/MuviApp.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 @main
 struct MuviApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  let persistenceController = PersistenceController.shared
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
+  }
 }
