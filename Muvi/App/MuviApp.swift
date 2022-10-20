3ddd1da795c8c3bcd2906e49665923241f65014d
Ibrahima Ciss <bionik6@gmail.com>
2022-10-20 04:09:05 +0100

    update main

--- Muvi/App/MuviApp.swift
+++ Muvi/App/MuviApp.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 @main
 struct MuviApp: App {
-  let persistenceController = PersistenceController.shared
   
   var body: some Scene {
     WindowGroup {
       ContentView()
-        .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
 
 // This code can be found in the Swift package:
 // https://github.com/johnno1962/HotSwiftUI
